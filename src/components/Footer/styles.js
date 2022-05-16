e7cc547915965ac13db8f688ce476ba871129291
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-16 16:10:23 -0300

    ajuste footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background: rgba(34, 34, 34, 0.6);
+  align-items: center;
+  background: #181818;
+  border-top: 1px solid #282828;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
   height: 100%;
+  justify-content: center;
+  padding: 0 1rem;
+  width: 100%;
 `
